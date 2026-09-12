f35952c665e604780647d5721db96d834410319f
Danny78-168 <zhu90305@gmail.com>
2026-09-13 06:32:18 +0800

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
 )
 
 # 你的 Google Apps Script 網頁應用程式網址
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzibIdfzlaqhDXYYdEPk49vV9h-TslfXTzbnCSE7j017iMTLN-XIvP8WMIerYDaqCk9/exec"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzCz5bJJjwh56ZA0Uc2NtZbKGeh_0RqNp2JWri5kfJaZomUT1e5gD-hezSRu8o_bQ2aow/exec"
 
 # 獎項與權重設定 (總權重 1000)
 prizes = [
